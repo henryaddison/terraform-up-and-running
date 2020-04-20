4e7c991ac5cf7925294e4f5564499ce60addc1f6
Henry Addison <henryaddison@users.noreply.github.com>
2020-04-20 06:10:42 +0100

    use a variable for frequently user port of web server

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -2,12 +2,19 @@ provider "aws" {
     region = "us-east-2"
 }
 
+variable "web_server_port" {
+    description = "Port on which web server runs"
+    type = number
+    default = 8080
+}
+
+
 resource "aws_security_group" "example_web_server_sg" {
     name = "terraform-example-web-server-sg"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.web_server_port
+        to_port = var.web_server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -24,7 +31,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
         #!/bin/bash
         echo "Hello, world" > index.html
-        nohup busybox httpd -f -p 8080&
+        nohup busybox httpd -f -p ${var.web_server_port} &
     EOF
 
     vpc_security_group_ids = [aws_security_group.example_web_server_sg.id]
