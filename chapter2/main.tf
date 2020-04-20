7b74ba47c7f789ed1639e3397faff5f65dbe4791
Henry Addison <henryaddison@users.noreply.github.com>
2020-04-20 05:53:41 +0100

    add a security group for web server access from whole world

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
     region = "us-east-2"
 }
 
+resource "aws_security_group" "example_web_server_sg" {
+    name = "terraform-example-web-server-sg"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
